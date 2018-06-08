025dc0a29641437b54df3539b4dcedf782f86d25
Jose Enrique Duran <eduran@mellow.cc>
2018-06-08 01:04:00 -0500

    Add models, migrations and README

---
+++ eya/utils/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+from model_utils.models import TimeStampedModel
+
+# Create your models here.
+class BaseModel(TimeStampedModel):
+	pass
+
+
+class CatalogModel(BaseModel):
+	name = models.CharField(verbose_name='Nombre', max_length=255)
+	display_name = models.CharField(verbose_name='Nombre a visualizar', max_length=255)
+
+	def __unicode__(self):
+		return u'{}'.format(display_name)
