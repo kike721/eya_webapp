3e22ac8f5d02ae069e2c25ff96a28ce1671c26be
Jose Enrique Duran <eduran@mellow.cc>
2018-06-19 00:48:37 -0500

    add fields is_new, spent and best_seller, add views for news and best_seller

--- eya/products/views.py
+++ eya/products/views.py
@@ -15,7 +15,61 @@ class IndexProducts(ListView):
 
     def get_queryset(self):
     	queryset = super(IndexProducts, self).get_queryset()
+    	queryset = queryset.filter(spent=False, is_new=False, best_seller=False)
+    	q = ''
     	if self.request.GET.has_key('q'):
     		q = self.request.GET['q']
     		queryset = queryset.filter(meta_description__icontains=q)
+    	self.q = q
     	return queryset
+
+    def get_context_data(self, **kwargs):
+    	context = super(IndexProducts, self).get_context_data(**kwargs)
+    	context['q'] = self.q
+    	context['news'] = Product.objects.filter(spent=False, is_new=True)
+    	context['best_seller'] = Product.objects.filter(spent=False, best_seller=True)
+    	return context
+
+
+class IndexNewProducts(ListView):
+
+    model = Product
+    paginate_by = 12
+    template_name = 'products/news_list.html'
+
+    def get_queryset(self):
+    	queryset = super(IndexNewProducts, self).get_queryset()
+    	queryset = queryset.filter(spent=False, is_new=True)
+    	q = ''
+    	if self.request.GET.has_key('q'):
+    		q = self.request.GET['q']
+    		queryset = queryset.filter(meta_description__icontains=q)
+    	self.q = q
+    	return queryset
+
+    def get_context_data(self, **kwargs):
+    	context = super(IndexNewProducts, self).get_context_data(**kwargs)
+    	context['q'] = self.q
+    	return context
+
+
+class IndexBestSellerProducts(ListView):
+
+    model = Product
+    paginate_by = 12
+    template_name = 'products/best_seller_list.html'
+
+    def get_queryset(self):
+    	queryset = super(IndexBestSellerProducts, self).get_queryset()
+    	queryset = queryset.filter(spent=False, best_seller=True)
+    	q = ''
+    	if self.request.GET.has_key('q'):
+    		q = self.request.GET['q']
+    		queryset = queryset.filter(meta_description__icontains=q)
+    	self.q = q
+    	return queryset
+
+    def get_context_data(self, **kwargs):
+    	context = super(IndexBestSellerProducts, self).get_context_data(**kwargs)
+    	context['q'] = self.q
+    	return context
