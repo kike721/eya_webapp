f1381ee5d8fd840c949629bd388df819f64c4d2b
Jose Enrique Duran <eduran@mellow.cc>
2018-09-15 16:30:02 -0500

    send emails order create

--- eya/store/email.py
+++ eya/store/email.py
@@ -9,13 +9,26 @@ from django.template.loader import render_to_string
 
 logger = logging.getLogger('eya')
 
-def email_investor_confirmation(order):
+def send_order_eya(order):
     """Send Order Data Email."""
     template = render_to_string(
     	'emails/send_data_order.html',
     	{'order': order, 'details': order.details.all()})
     email = generate_send_email(
-        ['jose.enrique.duran.garcia@gmail.com'],
+        # ['cotizaciones@ensamblesyadornos.org'],
+        ['jose.enrique.duran.garcia@gmail.com', 'valeria.pjaimes@gmail.com'],
+        "Orden #{}".format(order.pk),
+        template)
+
+    return handle_send_email([email])
+
+def send_order_customer(order):
+    """Send Order Data Email."""
+    template = render_to_string(
+    	'mailings/enviar_pedido.html',
+    	{'order': order, 'details': order.details.all()})
+    email = generate_send_email(
+        [order.customer.user.email],
         "Orden #{}".format(order.pk),
         template)
 
